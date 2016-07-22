8d585b0f4057f7420b256dc467a8dcc07bdda463
Koji Miyauchi <root@kmiyauchi.local.oicr.on.ca>
2016-07-22 20:40:25 +0000

    test

--- wfui-react-css.entry.js
+++ wfui-react-css.entry.js
@@ -1,35 +1,6 @@
-require('./legacy/css/fixed-data-table.min.scss');
-require('./legacy/css/react_animations.scss');
-
 require('./src/Description/description.scss');
 require('./src/InputField/input_field.scss');
 require('./src/InputTable/input_table.scss');
 require('./src/Selection/selection.scss');
 require('./src/Grid/grid.scss');
 require('./src/AddAnother/add_another.scss');
-
-/**
- * Old WFUI Components
- */
-//Button
-require('./src/Button/button.scss');
-
-//Dialog
-require('./legacy/css/wfui-layer.0.scss');
-require('./src/Dialog/dialog.scss');
-require('./src/Dialog/dialog2.scss');
-
-//Dropdown
-require('./src/Dropdown/dropdown.scss');
-
-//Tabs
-require('./src/Tabs/tabs.scss');
-
-//Label
-require('./src/Label/label.scss');
-
-//Tooltip
-require('./src/Tooltip/tooltip.scss');
-
-//Growl Alert
-require('./src/Growl/growl.scss');
