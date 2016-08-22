2e265cb6b3070bc0ae1927cd8583bf67a27d0236
Koji Miyauchi <root@kmiyauchi.local.oicr.on.ca>
2016-08-22 17:58:20 +0000

    Re-construct WFUI repository.

---
+++ wfui-css.entry.js
@@ -0,0 +1,25 @@
+require('./legacy/css/react_animations.scss');
+
+require('./src/Description/description.scss');
+require('./src/InputField/input_field.scss');
+require('./src/InputTable/input_table.scss');
+require('./src/Selection/selection.scss');
+require('./src/Grid/grid.scss');
+require('./src/AddAnother/add_another.scss');
+
+/**
+ * Old WFUI Components
+ */
+//Dialog
+require('./legacy/css/wfui-layer.0.scss');
+require('./src/Dialog/dialog.scss');
+require('./src/Dialog/dialog2.scss');
+
+//Label
+require('./src/Label/label.scss');
+
+//Tooltip
+require('./src/Tooltip/tooltip.scss');
+
+//Growl Alert
+require('./src/Growl/growl.scss');
--- wfui-react-css.entry.js
+++
@@ -1,6 +0,0 @@
-require('./src/Description/description.scss');
-require('./src/InputField/input_field.scss');
-require('./src/InputTable/input_table.scss');
-require('./src/Selection/selection.scss');
-require('./src/Grid/grid.scss');
-require('./src/AddAnother/add_another.scss');
