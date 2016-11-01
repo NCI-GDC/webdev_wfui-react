0e1becc9b7b3152e20f212cffbabf9335e904d79
root <root@koji.local.oicr.on.ca>
2016-11-01 19:53:12 +0000

    Try again

--- src/InputField/input_field.js
+++ src/InputField/input_field.js
@@ -27,6 +27,11 @@ class InputField extends Component {
     const {stopPropagation} = this.props;
     if(stopPropagation) e.stopPropagation();
   }
+  onHandleonFocus(e){
+    if(onHandleonFocus){
+      onHandleonFocus(e);
+    }
+  }
   onHandleChange(e){
     var val = e.target.value;
     
@@ -60,7 +65,7 @@ class InputField extends Component {
     var inputFieldElement = hideField ? "" :
           <span>
             {prefixField}
-              <input className={"wfui-input-field__input"+ errorClassName} type={type} defaultValue={defaultValue} value={value} placeholder={placeholder} name={name} onChange={this.onHandleChange.bind(this)} onClick={this.onHandleClick.bind(this)} onBlur={this.onHandleBlur.bind(this)} disabled={preview} maxLength={maxLength} min={min} max={max} />
+              <input className={"wfui-input-field__input"+ errorClassName} type={type} defaultValue={defaultValue} value={value} placeholder={placeholder} name={name} onFocus={this.onHandleonFocus.bind(this)} onChange={this.onHandleChange.bind(this)} onClick={this.onHandleClick.bind(this)} onBlur={this.onHandleBlur.bind(this)} disabled={preview} maxLength={maxLength} min={min} max={max} />
               {postfixField}
           </span>;
     return (
