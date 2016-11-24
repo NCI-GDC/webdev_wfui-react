6f8dd364d419a185cd4f35d361cc3bdb65a14f52
Ryan Annis <tinnymint@gmail.com>
2016-11-24 11:52:18 -0500

    added empty dropdown component

--- stories/Dropdown.js
+++ stories/Dropdown.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import TabbedList from '../src/Dropdown/Dropdown';
-import Tab from '../src/Dropdown/DropdownItem';
+import Dropdown from '../src/Dropdown/Dropdown';
+import DropdownItem from '../src/Dropdown/DropdownItem';
 
 storiesOf('Dropdown', module)
   .addWithInfo(
     'Basic',
     () => (
-        <TabbedList className="Basic">
-            <Tab className="Tab-Class" title="Tab 1">
-                Tab content
-            </Tab>
-            <Tab className="Tab-Class-2" title="Tab 2">
-                Second tab content
-            </Tab>
-        </TabbedList>
+        <div>
+            Empty dopdown Component
+        </div>
     ),
     { inline: true },
 );
