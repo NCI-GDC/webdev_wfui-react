58ff215a9ca174c66ea3bd2bb2b99c398555a14b
Ryan Annis <tinnymint@gmail.com>
2016-11-24 16:30:58 -0500

    removed dropdown

--- src/Dropdown/Dropdown.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Dropdown extends React.Component {
-    render() {
-        return <div />;
-    }
-}
-
-export default Dropdown;
--- src/Dropdown/DropdownItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class DropdownItem extends React.Component {
-    render() {
-        return <div />;
-    }
-}
-
-export default DropdownItem;
