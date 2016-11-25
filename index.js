07782aa9358495b9c92ea4fc3f2dd42f40082b23
Ryan Annis <tinnymint@gmail.com>
2016-11-25 11:33:16 -0500

    fixed npm exports

---
+++ index.js
@@ -0,0 +1,17 @@
+var FilteredList = reqiure('./lib/FilteredList/FilteredList').default;
+var Spinner = require('./lib/Spinner/Spinner');
+var TabbedList = require('./lib/TabbedList/TabbedList');
+var Tab = require('./lib/TabbedList/Tab');
+var TwitterFeed = require('./lib/TwitterFeed/TwitterFeed');
+var FilteredList = require('./lib/FilteredList/FilteredList');
+var FilteredTable = require('./lib/FilteredTable/FilteredTable');
+
+module.exports = {
+    FilteredList: FilteredList,
+    Spinner: Spinner,
+    TabbedList: TabbedList,
+    Tab: Tab,
+    TwitterFeed: TwitterFeed,
+    FilteredList: FilteredList,
+    FilteredTable: FilteredTable,
+}; 
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { FilteredList } from './FilteredList/FilteredList';
-export { Spinner } from './Spinner/Spinner';
-export { TabbedList } from './TabbedList/TabbedList';
-export { Tab } from './TabbedList/Tab';
-export { TwitterFeed } from './TwitterFeed/TwitterFeed';
