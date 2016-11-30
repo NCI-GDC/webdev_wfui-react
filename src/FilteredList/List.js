e491a65df5302eaae24231bd5df70ad2e423b85c
root <root@koji.local.oicr.on.ca>
2016-11-30 18:28:43 +0000

    Moved onNumOfListChange and onDisplay callback in componentDidUpdate, so that callback can do setState. (It was inside render() and didnt allow setState )

--- src/FilteredList/List.js
+++ src/FilteredList/List.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
+/* Calculates the list of articles that should be displayed on the current page */
+const calcActiveData = ({ data, pageSize, currentPage }) => {
+
+    const activeData = [];
+    const numArticles = data ? data.length : 0;
+
+    const startingArticle = pageSize * (currentPage - 1);
+    const lastArticle = Math.min(startingArticle + pageSize, numArticles);
+
+    for (let i = startingArticle; i < lastArticle; i += 1) {
+        activeData.push(data[i]);
+    }
+    return { activeData, startingArticle, lastArticle };
+};
+
 class List extends React.Component {
     render() {
-       const { itemDisplay, data, pageSize, currentPage, container, onDisplay, onNumOfListChange } = this.props;
-
-        /* Calculates the list of articles that should be displayed on the current page */
-        const activeData = [];
-        const numArticles = data ? data.length : 0;
-
-        const startingArticle = pageSize * (currentPage - 1);
-        const lastArticle = Math.min(startingArticle + pageSize, numArticles);
-
-        for (let i = startingArticle; i < lastArticle; i += 1) {
-            activeData.push(data[i]);
-        }
-
-        /* onDisplay is provided for cases that the client needs to see
-        * the range of articles being displayed */
-        onDisplay({ starting: startingArticle, last: lastArticle });
+        const { itemDisplay, data, pageSize, currentPage, container } = this.props;
 
         /* New article object with data injected into it. */
+        const { activeData } = calcActiveData(this.props);
         const itemDisplays = activeData.map((item, idx) => (
             React.cloneElement(
                 itemDisplay,
@@ -29,15 +30,21 @@ class List extends React.Component {
             )
         ));
 
-        /* Return number of item  */
-        onNumOfListChange(itemDisplays.length);
-
         /* Populates the container element passed to this with the items */
         const populatedContainer = React.cloneElement(
             container,
             { children: itemDisplays },
         );
         return populatedContainer;
+    }
+    componentDidUpdate() {
+        const { onDisplay, onNumOfListChange } = this.props;
+        const { activeData, startingArticle, lastArticle } = calcActiveData(this.props);
+        /* Return number of articles. */
+        onNumOfListChange(activeData.length);
+        /* onDisplay is provided for cases that the client needs to see
+        * the range of articles being displayed */
+        onDisplay({ starting: startingArticle, last: lastArticle });
     }
 }
 
