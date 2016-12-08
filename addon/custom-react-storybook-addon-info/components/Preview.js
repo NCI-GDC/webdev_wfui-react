76d40f4692c06158486021db468d4055b2c695b5
root <root@koji.local.oicr.on.ca>
2016-12-08 22:11:30 +0000

    update

--- addon/custom-react-storybook-addon-info/components/Preview.js
+++ addon/custom-react-storybook-addon-info/components/Preview.js
@@ -47,6 +47,42 @@ class Preview extends Component {
     clearTimeout(this.timeoutID); //eslint-disable-line no-undef
     this.timeoutID = setTimeout.apply(null, args); //eslint-disable-line no-undef
   }
+
+  _compileCode2() {
+    const { scope, code } = this.props;
+    try {
+      var compiledCode = transform(`
+        ((${Object.keys(scope).join(",")}) => (${code}));
+      `, { presets: ["es2015", "react", "stage-1"] }).code;
+      const tempScope = [];
+      Object.keys(scope).forEach(s => tempScope.push(scope[s]));
+      return eval(compiledCode).apply(null, tempScope)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  _renderCode() {
+    const mountNode = this.refs.mount;
+    const { scope, noRender, previewComponent } = this.props;
+    const tempScope = [];
+
+    try {
+      Object.keys(scope).forEach(s => tempScope.push(scope[s]));
+      tempScope.push(mountNode);
+      const compiledCode = this._compileCode();
+      const Comp = React.createElement(
+        eval(compiledCode).apply(null, tempScope)
+      );
+      console.log(Comp);
+      return Comp;
+      this.setState({ error: null });
+    } catch (err) {
+      this._setTimeout(() => {
+        this.setState({ error: err.toString() });
+      }, 500);
+    }
+  }
   _executeCode() {  
     const mountNode = this.refs.mount;
     const { scope, noRender, previewComponent } = this.props;
@@ -56,6 +92,7 @@ class Preview extends Component {
       Object.keys(scope).forEach(s => tempScope.push(scope[s]));
       tempScope.push(mountNode);
       const compiledCode = this._compileCode();
+      
       if (noRender) {
         /* eslint-disable no-eval, max-len */
         const Comp = React.createElement(
@@ -95,6 +132,7 @@ class Preview extends Component {
           <div className="playgroundError">{error}</div> :
           null}
         <div ref="mount" className="previewArea"/>
+        {this._compileCode2()}
       </div>
     );
   }
