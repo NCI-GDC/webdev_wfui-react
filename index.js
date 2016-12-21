06efb2044ffaa83709a62cfb074cfb34c8e022a7
root <root@koji.local.oicr.on.ca>
2016-12-21 16:24:56 +0000

    Removed tablist from index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var FilteredList = require('./lib/FilteredList/FilteredList').default;
 var Spinner = require('./lib/Spinner/Spinner').default;
-var TabbedList = require('./lib/TabbedList/TabbedList').default;
 var Tab = require('./lib/TabbedList/Tab').default;
 var TwitterFeed = require('./lib/TwitterFeed/TwitterFeed').default;
 var FilteredList = require('./lib/FilteredList/FilteredList').default;
@@ -9,7 +8,6 @@ var FilteredTable = require('./lib/FilteredTable/FilteredTable').default;
 module.exports = {
     FilteredList: FilteredList,
     Spinner: Spinner,
-    TabbedList: TabbedList,
     Tab: Tab,
     TwitterFeed: TwitterFeed,
     FilteredList: FilteredList,
