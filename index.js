dd91bada03c4206654586e692f1ebd77552a30a3
root <root@koji.local.oicr.on.ca>
2016-12-22 19:56:55 +0000

    Removed duplicate FilteredList from index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const FilteredList = require('./lib/FilteredList/FilteredList').default;
 const Spinner = require('./lib/Spinner/Spinner').default;
 const TwitterFeed = require('./lib/TwitterFeed/TwitterFeed').default;
-const FilteredList = require('./lib/FilteredList/FilteredList').default;
 const FilteredTable = require('./lib/FilteredTable/FilteredTable').default;
 const { Accordion, Alert, Badge, Breadcrumb, BreadcrumbItem, Button, ButtonGroup, ButtonToolbar, Carousel, CarouselItem, Checkbox, Clearfix, ControlLabel, Col, Collapse, Dropdown, DropdownButton, Fade, Form, FormControl, FormGroup, Glyphicon, Grid, HelpBlock, Image, InputGroup, Jumbotron, Label, ListGroup, ListGroupItem, Media, MenuItem, Modal, ModalBody, ModalFooter, ModalHeader, ModalTitle, Nav, Navbar, NavbarBrand, NavDropdown, NavItem, Overlay, OverlayTrigger, PageHeader, PageItem, Pager, Pagination, Panel, PanelGroup, Popover, ProgressBar, Radio, ResponsiveEmbed, Row, SafeAnchor, SplitButton, Tab, TabContainer, TabContent, Table, TabPane, Tabs, Thumbnail, Tooltip, Well } = require('react-bootstrap');
 
