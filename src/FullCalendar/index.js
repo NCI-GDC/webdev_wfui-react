5fe86b4d847ec80337f690a5111b6b0cb9e3bbca
Qingda Zou <qzou@olm-rannis.ad.oicr.on.ca>
2017-04-24 11:43:58 -0400

    FullCalendar fixes / props

--- src/FullCalendar/index.js
+++ src/FullCalendar/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import $ from 'jquery';
 import fullcalendar from 'fullcalendar';
-
+import './gcal.js';
+import './moment.js';
 
 // Finds active event source matching googleId, 
 // adds id and classname for styling purposes
@@ -39,22 +40,27 @@ function toggleEventSource(googleId, id, className) {
 //Handles event popup
 function eventClickHandler(event, jsEvent, view) {
     //Positioning from clicked element
+    var prevClass;
+    var currClass = jsEvent.currentTarget.classList[5];
+    var $eventPopup = $("#event-popup");
     var offset = $(this).offset();
-    var offsetX = $(this).width();
-    console.log("offsetX: " + offsetX);
-    var margin = 10;
-    $("#event-popup").css({
+    $eventPopup.css({
         "display": "initial",
-        "left": offset.left + margin + offsetX,
-        "top": offset.top,
+        "left": offset.left,
+        "top": offset.top - 35,
     });
+    if (prevClass) {
+		$eventPopup.removeClass(prevClass);
+	}
+	$eventPopup.addClass(currClass);
+	prevClass = currClass;
 
     //Getting event time formatting
-    var months = ["January", "February", "March","April","May","June","July","August","September","October","November","December"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var start = new Date(event.start._i);
     var printDate = days[start.getUTCDay()] + ", " + months[start.getUTCMonth()] + " " + start.getUTCDate() + ", " + start.getUTCFullYear();
-    if(event.end) {
+    if (event.end) {
         var end = new Date(event.end._i);
         var startMinutes = start.getUTCMinutes();
         var endMinutes = end.getUTCMinutes();
@@ -65,7 +71,7 @@ function eventClickHandler(event, jsEvent, view) {
     $("#event-link").attr('href', event.url);
     $("#event-link").html(event.title);
     $("#event-time").html(printDate + "<br/>" + printTime);
-    console.log(event.start);
+    //console.log(event.start);
     if (event.description) {
         $("#event-description").html(event.description);
     }
@@ -85,35 +91,28 @@ function eventClickHandler(event, jsEvent, view) {
 
 //Inserts "UTC"
 function eventRenderHandler(event, element) {
-    console.log(element);
+    //console.log(element);
     element.find(".fc-time").append("<span class=\"timezone\"> UTC</span>");
 }
 
-// Placeholder calendars
-var allSources = [
-    {
-        googleCalendarId: 'rs25l6f24k3bih03rqhha3snf8@group.calendar.google.com',
-        id: 'calendar-1',
-        className: 'event-source-1',
-        timezone: 'UTC'
-    },
-    {
-        googleCalendarId: 'est9cq04ufnc05eg3qoq0ig78s@group.calendar.google.com',
-        id: 'calendar-2',
-        className: 'event-source-2',
-        timezone: 'UTC'
-    }
-];
-
-
 export default class PNotify extends React.Component {
     componentDidMount() {
-        console.log($.fn);
-        console.log(fullcalendar);
+        //console.log($.fn);
+        //console.log(fullcalendar);
+        const { sources, googleApiKey } = this.props;
+        var allSources = [];
+        for (let i = 0; i < sources.length; i++) {
+            allSources.push({
+                googleCalendarId: sources[i].googleCalendarId,
+                id: sources[i].id,
+                className: 'event-source-' + (i + 1),
+                timezone: 'UTC'
+            });
+        }
 
         $('#calendar').fullCalendar({
             //Temporary API key
-            googleCalendarApiKey: 'AIzaSyBxG93xOFVuPVV69vsPNCBet9vE4LKyDnw',
+            googleCalendarApiKey: googleApiKey,
             eventSources: allSources,
             timezone: 'UTC',
             timeFormat: 'H(:mm)',
@@ -129,40 +128,63 @@ export default class PNotify extends React.Component {
         });
 
         //Toggles source, refreshes calendar
-        $("#toggle-1").on("click", function () {
-            toggleEventSource(allSources[0].googleCalendarId, allSources[0].id, allSources[0].className);
-        });
-        $("#toggle-2").on("click", function () {
-            toggleEventSource(allSources[1].googleCalendarId, allSources[1].id, allSources[1].className);
-        });
-    
-    //Closes event popup if click anywhere outside of it
-        $(document).on("click",function(e) {
-            if(!($(e.target).parents("#event-popup").length > 0) && !($(e.target).parents(".fc-day-grid-event").length > 0)) {
+        for (let i = 0; i < allSources.length; i++) {
+            var toggleNum = "#toggle-" + (i + 1);
+            $(toggleNum).on("click", function () {
+                toggleEventSource(allSources[i].googleCalendarId, allSources[i].id, allSources[i].className);
+            });
+        }
+
+        //Closes event popup if click anywhere outside of it
+        $(document).on("click", function (e) {
+            if (!($(e.target).parents("#event-popup").length > 0) && !($(e.target).parents(".fc-day-grid-event").length > 0)) {
                 $("#event-popup").css("display", "none");
             }
         });
     }
+
+    renderToggleLabels(sources) {
+        var toggleLabels = [];
+        for (let i = 0; i < sources.length; i++) {
+            var name = sources[i].id;
+            toggleLabels.push((
+                <label className="checkbox-inline" idx={i}>
+                    <input type="checkbox" id={"toggle-"+(i+1)} value={"show-"+(i+1)} defaultChecked="true"></input>{name}
+                </label>
+            ));
+        }
+        return toggleLabels;
+    }
+
     render() {
         return (
-            <div>
-                <div id="topbar">
-                    <span>Working Group Calendars</span>
-                    <div id="toggle-bar">
-                        <div id="toggle-1">Toggle 1</div>
-                        <div id="toggle-2">Toggle 2</div>
-                        <div id="toggle-3">Toggle 3</div>
-                        <div id="toggle-4">Toggle 4</div>
-                    </div>
-                </div>        
-                <div id="calendar">
-                    <div id="event-popup">
-                        <a id="event-link" target="_blank"></a>
-                        <div id="event-time"></div>
-                        <span id="event-location"></span>
-                        <span id="event-description"></span>
+            <div className="container-fluid">
+                <div className="row">
+                    <header className="header col-md-12 clearfix">
+                        <h1 className="pull-left">Working Group Calendars</h1>
+                        <div className="toggle-bar pull-right">
+                            <span>Show only:</span>
+                            {this.renderToggleLabels(this.props.sources)}
+                        </div>
+                    </header>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <div id="calendar" className="calendar">
+                            <div id="event-popup" className="event-popup">
+                                <div>
+                                    <h5><a id="event-link" className="event-link" target="_blank"></a></h5>
+                                    <div id="event-day" className="event-day"></div>
+                                    <div id="event-time" className="event-time"></div>
+                                    <div id="event-location" className="event-location"></div>
+                                    <div id="event-description" className="event-description"></div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
         )
     }
