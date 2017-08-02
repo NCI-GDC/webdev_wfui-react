e32981e08cae0863c63b184e3daddb26fa5bcf00
Koji Miyauchi <root@jessie.raw>
2017-08-02 14:35:33 -0400

    Added ModalDialog component

---
+++ src/ModalDialog/selector.js
@@ -0,0 +1,9 @@
+import { createSelector } from 'reselect';
+
+const _modalsSelector = state => state.modals;
+export const modalsSelector = key => (
+    createSelector(
+        _modalsSelector,
+        modals => (modals[key]),
+    )
+);
