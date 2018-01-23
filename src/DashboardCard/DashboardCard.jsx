51fb0e0c2332e90978c66fceca2c7e9a3e92beed
Kyle Kim <kkim@olw-szheng4431.ad.oicr.on.ca>
2018-01-23 11:18:27 -0500

    Dashboard Card Update

--- src/DashboardCard/DashboardCard.jsx
+++ src/DashboardCard/DashboardCard.jsx
@@ -2,6 +2,7 @@ import React, { cloneElement } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Card from '../Card/Card';
+import { stringifyValues } from '../util/stringifyValues';
 
 const DashboardCardBody = ({ role, className, children }) => (
     <div role={role} className={classNames(className, 'form-box-body')}>
@@ -42,16 +43,32 @@ DashboardCardFooter.defaultProps = {
     hover: false,
 };
 
+const DashboardCardFilterData = ({ role, data }) => (
+    <span role={role} className="hide isotope-search" aria-hidden="true" hidden>
+        {stringifyValues(data)}
+    </span>
+);
+
+DashboardCardFilterData.propTypes = {
+    role: PropTypes.string,
+    data: PropTypes.object,
+};
+
+DashboardCardFilterData.defaultProps = {
+    role: 'filterData',
+};
+
 const BODY_ROLE = DashboardCardBody.defaultProps.role;
 const FOOTER_ROLE = DashboardCardFooter.defaultProps.role;
+const DATA_ROLE = DashboardCardFilterData.defaultProps.role;
 
 class DashboardCard extends React.Component {
     render() {
-        const { className, children, cardStyle } = this.props;
+        const { className, children, data, cardStyle } = this.props;
 
         return (
-            <Card className={classNames(className, 'form-box')} cardStyle={cardStyle}>
-                <Card.Body>
+            <Card className={classNames(className, 'form-box-container')}>
+                <Card.Body className="form-box">
                     {children &&
                         ((Array.isArray(children) &&
                             children.map((child, key) => {
@@ -64,6 +81,7 @@ class DashboardCard extends React.Component {
                                 }
                             })) ||
                             cloneElement(children))}
+                    {data && <DashboardCardFilterData data={data} />}
                 </Card.Body>
             </Card>
         );
@@ -73,7 +91,7 @@ class DashboardCard extends React.Component {
 DashboardCard.propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
-    cardStyle: PropTypes.object,
+    data: PropTypes.object,
 };
 
 DashboardCard.Body = DashboardCardBody;
