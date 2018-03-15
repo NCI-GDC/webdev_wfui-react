80e5f7089e49b22f3126feac8bd54fe001ede634
Koji Miyauchi <root@jessie.raw>
2018-03-15 11:30:46 -0400

    LoadingComponent: Bug fix

--- src/LoadingComponent/LoadingComponent.js
+++ src/LoadingComponent/LoadingComponent.js
@@ -133,7 +133,7 @@ LoadingComponent.propTypes = {
     fetch5s: PropTypes.bool,
     fetch8s: PropTypes.bool,
     status: PropTypes.string,
-    error: PropTypes.oneOfType([PropTypes.string, PropTypes.string]),
+    error: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
     timeout: PropTypes.bool,
     retried: PropTypes.bool,
     hideMessage: PropTypes.bool,
