af0346c538bf0ae183b849f55cf9f5ad4a326e27
Koji Miyauchi <root@jessie.raw>
2018-03-16 10:23:07 -0400

    Update utils

--- src/util.js
+++ src/util.js
@@ -2,10 +2,12 @@ import * as wfuiFetch from './util/wfuiFetch';
 import { extLink } from './util/jquery.extLink';
 import { scrollToFirstError } from './util/scrollToFirstError';
 import { stringifyValues } from './util/stringifyValues';
+import { flattenObject } from './util/flattenObject';
 
 module.exports = {
     ...wfuiFetch,
     extLink,
     scrollToFirstError,
     stringifyValues,
+    flattenObject,
 };
