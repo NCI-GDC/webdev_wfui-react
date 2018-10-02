29a1907385efe32751184d81f3782060b12ba86d
Kyle Kim <kyle.kim@oicr.on.ca>
2018-10-02 15:47:52 -0400

    Update

--- src/ThreeColumnView/ThreeColumnView.jsx
+++ src/ThreeColumnView/ThreeColumnView.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import SplitPane from 'react-split-pane'; // import SplitPane from 'react-split-pane';
+import SplitPane from 'react-split-pane';
 
 import Column from './Column';
 
