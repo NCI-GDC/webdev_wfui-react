bcb6fc467baea9fa3230d00ca7f487a667be00cd
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-10-17 15:47:59 -0400

    In progress

--- src/stories/Editor/index.stories.js
+++ src/stories/Editor/index.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/react';
 import StoryBasicExample from './StoryBasicExample';
 
-// storiesOf('Editor', module).add(
-//     'React DraftJS Editor conversion of content from and to HTML',
-//     () => StoryBasicExample
-// );
+storiesOf('Editor', module).add(
+    'React DraftJS Editor conversion of content from and to HTML',
+    () => StoryBasicExample
+);
