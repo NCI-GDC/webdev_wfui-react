a6a2c674140103c1eb88cbc9197f78f61992ccc3
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-10-18 15:53:59 -0400

    Set up building environment

---
+++ buildConfigs/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/env',
+            {
+                targets: {
+                    browsers: ['>0.25%, not dead'],
+                },
+                modules: false,
+            },
+        ],
+    ],
+    plugins: [
+        ['@babel/proposal-object-rest-spread', { loose: true }],
+        ['@babel/plugin-proposal-decorators', { legacy: true }],
+        '@babel/plugin-transform-react-jsx',
+    ],
+};
---
+++ scripts/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#rm -rf dist
+#./node_modules/.bin/webpack
+#./node_modules/.bin/webpack --output-filename=dist/WFUI.js
+#./node_modules/.bin/webpack --output-filename=dist/WFUI.min.js --optimize-minimize
+
+rm -rf lib
+npx babel --verbose --config-file=./buildConfigs/babel.config.js src/components --out-dir lib
+
+# rm -rf dist
+./node_modules/.bin/webpack --quiet --config ./buildConfigs/webpack.config.js --mode=production
