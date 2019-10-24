8a31bdfc6560b39c15edc2cfcafd603e4b6f2e1c
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-10-24 17:28:06 -0400

    Update Icon

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +7,11 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 library.add(fas);
 
 const Icon = props => {
-    return <FontAwesomeIcon icon={props.icon || props.name || props.glyph} />;
+    return (
+        <FontAwesomeIcon
+            className={props.className}
+            icon={props.icon || props.name || props.glyph}
+        />
+    );
 };
 export default Icon;
