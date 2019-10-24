5b3d216c61c94d733104aaf65a20bc470c155282
Koji Miyauchi <kmiyauchi@odm-kmiyauchi.ad.oicr.on.ca>
2019-10-24 17:12:16 -0400

    Add icon asset

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
+library.add(fas);
+
 const Icon = props => {
     return <FontAwesomeIcon icon={props.icon || props.name || props.glyph} />;
 };
