fb575c1c5dd11d76fd692c83de2cead470a5b147
Koji Miyauchi <koji.miyauchi@oicr.on.ca>
2020-07-27 21:33:04 -0400

    Update

--- src/components/FormFields/mutators.jsx
+++ src/components/FormFields/mutators.jsx
@@ -5,3 +5,19 @@ export const setFieldUntouched = (args, state) => {
         field.touched = false;
     }
 };
+
+export const setFieldUnmodified = (args, state) => {
+    const [name] = args;
+    const field = state.fields[name];
+    if (field) {
+        field.modified = false;
+    }
+};
+
+export const setFieldUndirty = (args, state) => {
+    const [name] = args;
+    const field = state.fields[name];
+    if (field) {
+        field.dirty = false;
+    }
+};
